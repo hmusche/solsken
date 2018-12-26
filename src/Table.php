a279cd62eb54a05fcee1b07537b9a11741c80024
Hendrick <hmusche@gmail.com>
2018-12-26 01:24:26 +0000

    add actions to table

--- src/Table.php
+++ src/Table.php
@@ -7,6 +7,7 @@ use Solsken\Table\Column;
 class Table {
     protected $_columns = [];
     protected $_data = [];
+    protected $_actions = [];
 
     public function __construct() {
 
@@ -30,6 +31,16 @@ class Table {
         return $this;
     }
 
+    public function addAction(string $key, array $action) {
+        $this->_actions[$key] = $action;
+
+        return $this;
+    }
+
+    public function hasActions() {
+        return $this->_actions !== [];
+    }
+
     public function setData($data) {
         $this->_data = $data;
     }
@@ -43,6 +54,22 @@ class Table {
 
         foreach ($this->_data as $dataRow) {
             $row = [];
+
+            if ($this->_actions !== []) {
+                $actions = [];
+
+                foreach ($this->_actions as $key => $action) {
+                    preg_match('#{(.+)}#', $action['href'], $match);
+
+                    $action['href'] = isset($match[1]) && isset($dataRow[$match[1]])
+                                    ? str_replace($match[0], $dataRow[$match[1]], $action['href'])
+                                    : '#';
+
+                    $actions[$key] = $action;
+                }
+
+                $row[] = $actions;
+            }
 
             foreach ($this->_columns as $column) {
                 $row[] = $column->getValue($dataRow);
