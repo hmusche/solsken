d418c11e7f1b6f402595a0048efccf1727b123f5
Hendrick <hmusche@gmail.com>
2019-01-06 10:30:21 +0100

    add preview method to file element

--- src/Form/Element/File.php
+++ src/Form/Element/File.php
@@ -4,10 +4,19 @@ namespace Solsken\Form\Element;
 
 use Solsken\Form\ElementAbstract;
 
+use Solsken\View;
+
 class File extends ElementAbstract {
     protected $_template = 'file';
 
     public function populate(Array $data) {
         $this->_value = array_key_exists($this->_name, $data) ? $data[$this->_name] : null;
+    }
+
+    public function getPreview() {
+        $type = isset($this->_options['preview']) ? $this->_options['preview'] : 'direct';
+        $view = View::getInstance();
+
+        return $view->partial('partial/element/file/' . $type . '.phtml', ['value' => $this->_value]);
     }
 }
