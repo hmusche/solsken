282922d8526063ee8ef84362405ff22852d52e85
Hendrick <hmusche@gmail.com>
2019-01-17 22:52:54 +0100

    add Date form element

---
+++ src/Form/Element/Date.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Solsken\Form\Element;
+
+use Solsken\Form\Element\Text;
+
+class Date extends Text {
+    protected $_template = 'text';
+
+    protected $_attributes = [
+        'type' => 'text',
+        'data-type' => 'date'
+    ];
+
+    public function populate(Array $data) {
+        $this->_value = array_key_exists($this->_name, $data) ? $data[$this->_name] : null;
+    }
+}
