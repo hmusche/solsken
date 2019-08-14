07732fc29f34cd9d7777253a457e34480867c02b
Hendrick <hmusche@gmail.com>
2019-08-14 11:40:04 +0700

    fix select options not showing on empty form

--- src/Form/Element/Select.php
+++ src/Form/Element/Select.php
@@ -9,11 +9,27 @@ class Select extends ElementAbstract {
 
     protected $_values = [];
 
-    public function getSelect() {
-        return $this->_values;
+    /**
+     * Overwrite Parent select to prefill select option
+     * @param string $name
+     * @param array  $options [description]
+     * @param string $value   [description]
+     */
+    public function __construct($name, $options, $value = null) {
+        parent::__construct($name, $options, $value);
+
+        $this->getSelect();
     }
 
-    public function populate(Array $data) {
+    /**
+     * Return all options of select
+     * @return array
+     */
+    public function getSelect() {
+        if ($this->_values) {
+            return $this->_values;
+        }
+
         $values = [];
 
         if (isset($this->_options['values'])) {
@@ -21,6 +37,11 @@ class Select extends ElementAbstract {
         }
 
         $this->_values = $values;
-        $this->_value  = array_key_exists($this->_name, $data) && array_key_exists($data[$this->_name], $values) ? $data[$this->_name] : null;
+
+        return $this->_values;
+    }
+
+    public function populate(Array $data) {
+        $this->_value  = array_key_exists($this->_name, $data) && array_key_exists($data[$this->_name], $this->_values) ? $data[$this->_name] : null;
     }
 }
